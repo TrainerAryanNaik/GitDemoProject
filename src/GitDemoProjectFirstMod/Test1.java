eceeaf09041ee3523e11caeeab3f290097bf8a75
TrainerAryanNaik <traineraryan89@gmail.com>
2022-03-09 11:11:41 +0530

    first commit...

---
+++ src/GitDemoProjectFirstMod/Test1.java
@@ -0,0 +1,14 @@
+package GitDemoProjectFirstMod;
+public class Test1 {
+
+	public static void main(String[] args) {
+		int a=5,sum=0;
+		for(int i=0;i<a;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.println("sum="+sum);
+
+	}
+
+}
